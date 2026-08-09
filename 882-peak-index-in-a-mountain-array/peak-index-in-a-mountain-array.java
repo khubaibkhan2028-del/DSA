89e512f7df4a64acfbbdc337b577dfb9de29019c
khubaibkhan2028-del <khubaibgcet2028@gmail.com>
2026-08-09 23:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 80.4 MB (41.04%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -2,33 +2,33 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         // first method.....
 
-        int n = arr.length;
-        int lo = 0, hi = n - 1;
-        while (lo <=hi) {
-            int mid = lo + (hi - lo) / 2;
-            //  increasing side
-            if (arr[mid] < arr[mid + 1]) {
-                lo = mid + 1;
-            }
-            //  decreasing side
-            else {
-                hi = mid-1;
-            }
-        }
+        // int n = arr.length;
+        // int lo = 0, hi = n - 1;
+        // while (lo <=hi) {
+        //     int mid = lo + (hi - lo) / 2;
+        //     //  increasing side
+        //     if (arr[mid] < arr[mid + 1]) {
+        //         lo = mid + 1;
+        //     }
+        //     //  decreasing side
+        //     else {
+        //         hi = mid-1;
+        //     }
+        // }
 
-        return lo;
+        // return lo;
 
         // second method....
 
-        // int n = arr.length;
-        // int lo=0, hi=n-1;
-        // while(lo<=hi){
-        //     int mid =(lo+hi)/2;
-        //     if(mid>0 && mid<n-1 && arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) return mid;
-        //     else if(mid>0 && mid<n-1 && arr[mid]>arr[mid-1]&& arr[mid]<arr[mid+1]) lo=mid+1;
-        //     else hi=mid;
-        // }
-        // return -1;
+        int n = arr.length;
+        int lo=0, hi=n-1;
+        while(lo<=hi){
+            int mid =lo + (hi - lo) / 2;
+            if(mid>0 && mid<n-1 && arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) return mid;
+            else if(mid>0 && mid<n-1 && arr[mid]>arr[mid-1]&& arr[mid]<arr[mid+1]) lo=mid+1;
+            else hi=mid;
+        }
+        return -1;
 
 
         // third method
