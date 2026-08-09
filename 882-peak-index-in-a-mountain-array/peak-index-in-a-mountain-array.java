15315b5d2cd0f89623c784c3f58151095d76f902
khubaibkhan2028-del <khubaibgcet2028@gmail.com>
2026-08-09 23:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 80.1 MB (78.59%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -2,21 +2,21 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         // first method.....
 
-        // int n = arr.length;
-        // int lo = 0, hi = n - 1;
-        // while (lo < hi) {
-        //     int mid = lo + (hi - lo) / 2;
-        //     //  increasing side
-        //     if (arr[mid] < arr[mid + 1]) {
-        //         lo = mid + 1;
-        //     }
-        //     //  decreasing side
-        //     else {
-        //         hi = mid;
-        //     }
-        // }
+        int n = arr.length;
+        int lo = 0, hi = n - 1;
+        while (lo <=hi) {
+            int mid = lo + (hi - lo) / 2;
+            //  increasing side
+            if (arr[mid] < arr[mid + 1]) {
+                lo = mid + 1;
+            }
+            //  decreasing side
+            else {
+                hi = mid-1;
+            }
+        }
 
-        // return lo;
+        return lo;
 
         // second method....
 
@@ -30,14 +30,16 @@ class Solution {
         // }
         // return -1;
 
-        int n = arr.length;
-        int lo=1, hi=n-2;
-        while(lo<=hi){
-            int mid =(lo+hi)/2;
-            if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) return mid;
-            else if(arr[mid]>arr[mid-1]&& arr[mid]<arr[mid+1]) lo=mid+1;
-            else hi=mid;
-        }
-        return -1;
+
+        // third method
+        // int n = arr.length;
+        // int lo=1, hi=n-2;
+        // while(lo<=hi){
+        //     int mid =(lo+hi)/2;
+        //     if(arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1]) return mid;
+        //     else if(arr[mid]>arr[mid-1]&& arr[mid]<arr[mid+1]) lo=mid+1;
+        //     else hi=mid;
+        // }
+        // return -1;
     }
 }
