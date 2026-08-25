a041827a8f4ad65e00631d0d9940ba60ac8e5e82
khubaibkhan2028-del <khubaibgcet2028@gmail.com>
2026-08-25 13:46:51 +0530

    Time: 5 ms (98.30%) | Memory: 43.2 MB (29.33%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;       
+        for (int i = 0; i < s.length(); i++) {
+            count += expandAroundCenter(s, i, i);
+            count += expandAroundCenter(s, i, i + 1);
+        }      
+        return count;
+    }  
+    private int expandAroundCenter(String s, int left, int right) {
+        int palindromes = 0;       
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            palindromes++;
+            left--;
+            right++;
+        }   
+        return palindromes;
+    }
+}
