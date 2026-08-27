ac467ff256fca2ecebf2e7b8616886c585a5bdf3
khubaibkhan2028-del <khubaibgcet2028@gmail.com>
2026-08-27 21:47:02 +0530

    Time: 6 ms (94.90%) | Memory: 45.2 MB (29.33%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -4,14 +4,17 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             strs[i] = String.valueOf(nums[i]);
         }
+
         Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
         if (strs[0].equals("0")) {
             return "0";
         }
+
         StringBuilder sb = new StringBuilder();
         for (String str : strs) {
             sb.append(str);
         }
+        
         return sb.toString();
     }
 }
